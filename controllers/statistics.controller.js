d4cca9f283dcb2d1a770f4ab6afa480704c47637
Kirill Petrosian <kiril.petrosian@wsc-sports.com>
2022-04-27 20:56:48 +0400

    all time statistics

--- controllers/statistics.controller.js
+++ controllers/statistics.controller.js
@@ -11,6 +11,8 @@ import Garment from "../model/garment.model.js";
 const STATISTICS_RADIUS_KM = 50;
 const LATITUDES_IN_KILOMETER = 1 / 111;
 const LONGITUDES_IN_KILOMETER = 1 / 95;
+const TEMPERATURE_DIFF = 1;
+const WIND_SPEED_DIFF = 0.5;
 
 export const getToday = async (req, res, next) => {
   const {
@@ -34,6 +36,99 @@ export const getToday = async (req, res, next) => {
       [Op.and]: [
         { latitude: { [Op.between]: latitudeRange } },
         { longitude: { [Op.between]: longitudeRange } },
+      ],
+    },
+    include: "userOutfits",
+  }).then((weathers) =>
+    weathers.map(({ userOutfits }) => userOutfits.map(({ id }) => id)).flat()
+  );
+
+  const outfitsGarmentsIds = await OutfitGarment.findAll({
+    where: { outfitId: { [Op.in]: outfitsIds } },
+  }).then((outfitsGarments) =>
+    outfitsGarments.map(({ outfitId, garmentId }) => ({ outfitId, garmentId }))
+  );
+
+  const garments = await Garment.findAll({
+    where: {
+      id: {
+        [Op.in]: outfitsGarmentsIds.map(({ garmentId }) => garmentId),
+      },
+    },
+  });
+
+  const outfits = outfitsGarmentsIds.reduce(
+    (res, { outfitId, garmentId }) => ({
+      ...res,
+      [outfitId]: [
+        ...(res[outfitId] || []),
+        garments.find(({ id }) => id === garmentId),
+      ],
+    }),
+    {}
+  );
+
+  const garmentsPoints = garments.reduce(
+    (res, { id }) => ({
+      ...res,
+      [id]: outfitsGarmentsIds.filter(({ garmentId }) => garmentId === id)
+        .length,
+    }),
+    {}
+  );
+
+  const outfitsPoints = Object.entries(outfits).reduce(
+    (res, [outfitId, garments]) => ({
+      ...res,
+      [outfitId]: garments.reduce((sum, { id }) => sum + garmentsPoints[id], 0),
+    }),
+    {}
+  );
+
+  const mostPopularOutfitId = Object.entries(outfitsPoints).reduce(
+    (maxId, [outfitId, points]) =>
+      !maxId || outfitsPoints[maxId] < points ? outfitId : maxId,
+    null
+  );
+  const mostPopularOutfit = outfits[mostPopularOutfitId];
+
+  return res.status(200).json({ outfits, mostPopularOutfit });
+};
+
+export const getAllTime = async (req, res, next) => {
+  const {
+    user: { id: userId },
+    temp,
+    windSpeed,
+    radius = STATISTICS_RADIUS_KM,
+    coordinates: [latitude, longitude],
+  } = req.body;
+
+  const latitudeRange = [
+    latitude - LATITUDES_IN_KILOMETER * radius,
+    latitude + LATITUDES_IN_KILOMETER * radius,
+  ];
+  const longitudeRange = [
+    longitude - LONGITUDES_IN_KILOMETER * radius,
+    longitude + LONGITUDES_IN_KILOMETER * radius,
+  ];
+  const tempRange = [temp - TEMPERATURE_DIFF, temp + TEMPERATURE_DIFF];
+  const windSpeedRange = [
+    windSpeed - WIND_SPEED_DIFF,
+    windSpeed + WIND_SPEED_DIFF,
+  ];
+
+  const outfitsIds = await Weather.findAll({
+    where: {
+      [Op.and]: [
+        { latitude: { [Op.between]: latitudeRange } },
+        { longitude: { [Op.between]: longitudeRange } },
+        {
+          airTemperature: {
+            [Op.between]: tempRange,
+          },
+        },
+        { windSpeed: { [Op.between]: windSpeedRange } },
       ],
     },
     include: "userOutfits",
--- routes/statistics.router.js
+++ routes/statistics.router.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 
 import { parseUser } from "../controllers/shared.controller.js";
-import { getToday } from "../controllers/statistics.controller.js";
+import { getAllTime, getToday } from "../controllers/statistics.controller.js";
 
 const router = Router();
 
 router.post("/get-today", parseUser, getToday);
+
+router.post("/get-all-time", parseUser, getAllTime);
 
 export default router;
